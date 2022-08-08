bd3297acece737f8a78224a8bdeaae6794f38d34
Binkitubo <davidlolleria@hotmail.com>
2022-08-08 15:22:11 +0000

    Finished code for the random card and styles

--- src/app.js
+++ src/app.js
@@ -2,10 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const palo = ["♦", "♥", "♠", "♣"];
+const numero = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "K",
+  "Q",
+  "J",
+  "A"
+];
+
+function cartaRandom(array) {
+  let randomPosition = Math.floor(Math.random() * array.length);
+  return array[randomPosition];
+}
+
+const suit = document.querySelector(".palo");
+const suit2 = document.querySelector(".palo2");
+const number = document.querySelector(".numero");
+let randomSuit = cartaRandom(palo);
+suit.innerHTML = randomSuit;
+suit2.innerHTML = randomSuit;
+number.innerHTML = cartaRandom(numero);
+
+function color() {
+  if (randomSuit === "♦") {
+    suit.classList.add("diamante");
+  } else if (randomSuit === "♥") {
+    suit.classList.add("corazon");
+  } else if (randomSuit === "♠") {
+    suit.classList.add("picas");
+  } else {
+    suit.classList.add("trebol");
+  }
+}
+
+function color2() {
+  if (randomSuit === "♦") {
+    suit2.classList.add("diamante");
+  } else if (randomSuit === "♥") {
+    suit2.classList.add("corazon");
+  } else if (randomSuit === "♠") {
+    suit2.classList.add("picas");
+  } else {
+    suit2.classList.add("trebol");
+  }
+}
+
+let resultado = randomSuit + color();
+let resultado2 = randomSuit + color2();
